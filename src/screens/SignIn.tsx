60c0d02e38ea8a3c5863ffc87e9336ac55d929c1
Luis Murilo <luiszkm@gmail.com>
2022-11-02 13:45:11 -0300

    nlw1/5 finished

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,17 @@
+import { Center, Text } from "native-base";
+
+
+
+
+export function SignIn() {
+
+
+
+  return (
+    <Center flex={1} bgColor="gray.900r">
+      <Text color={"white"} fontSize={24}>
+        SignIn
+      </Text>
+    </Center>
+  )
+}
