115e17df32ee65ff476fed3c7b6aa33af12573b0
Luis Murilo <luiszkm@gmail.com>
2022-11-03 20:54:58 -0300

    add button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,9 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import {Fontisto} from '@expo/vector-icons'
 
 
+import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button";
 
 
 export function SignIn() {
@@ -8,10 +11,13 @@ export function SignIn() {
 
 
   return (
-    <Center flex={1} bgColor="gray.900r">
-      <Text color={"white"} fontSize={24}>
-        SignIn
-      </Text>
+    <Center flex={1} bgColor="gray.900">
+      <Logo width={212} height={40} />
+
+      <Button 
+      title="Entrar com google"
+       type="SECONDARY"
+      leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>} />
     </Center>
   )
 }
