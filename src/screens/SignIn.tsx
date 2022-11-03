c11609851480a5c7bf2055229b64ba52b7e63c8c
Luis Murilo <luiszkm@gmail.com>
2022-11-03 20:59:13 -0300

    screen login finished

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,5 @@
 import { Center, Text, Icon } from "native-base";
-import {Fontisto} from '@expo/vector-icons'
+import { Fontisto } from '@expo/vector-icons'
 
 
 import Logo from '../assets/logo.svg'
@@ -11,13 +11,21 @@ export function SignIn() {
 
 
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
-      <Button 
-      title="Entrar com google"
-       type="SECONDARY"
-      leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>} />
+      <Button
+        type="SECONDARY"
+        title="Entrar com google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+
+      <Text color='white' textAlign='center' mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   )
 }
