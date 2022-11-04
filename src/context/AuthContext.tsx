e49dc614d736fc72e86cd1bce7a22fe4a876a0bc
Luis Murilo <luiszkm@gmail.com>
2022-11-04 00:08:57 -0300

    add google auth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,10 @@
-import { createContext, ReactNode } from "react";
-import * as AuthSession from 'expo-auth-session'
+import { createContext, ReactNode, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession()
+
 
 interface UserProps {
   name: string;
@@ -23,11 +28,23 @@ export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
 
+  const [isUserLoading, setIsUserLoading] = useState()
 
-  AuthSession.makeRedirectUri({useProxy:true})
+ const [ request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '848727285664-47vt9khtu7hr22n0mlq3flcd28ucb563.apps.googleusercontent.com' ,
+    redirectUri:   AuthSession.makeRedirectUri({useProxy:true}),
+    scopes: [ 'profile', 'email']
+
+  })
+
+
 
   async function signIn() {
+    try{
 
+    }catch (error) {
+
+    }
 
   }
 
